ee7eb2bb8f2cf0d94daa8eff91ed7999df9d0e80
leromayagg <leromayagg@gmail.com>
2024-03-12 08:59:33 -0300

    feat JPA

--- src/main/java/fiap/com/br/flashshopco/controller/ProdutoController.java
+++ src/main/java/fiap/com/br/flashshopco/controller/ProdutoController.java
@@ -5,12 +5,9 @@ import fiap.com.br.flashshopco.repository.ProdutoRepository;
 
 import java.util.*;
 
-import org.apache.catalina.startup.ClassLoaderFactory.Repository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
